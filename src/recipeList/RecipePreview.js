5fcbbdf75c7757022f657c0328cad517ff86ac93
psclahr <48948915+psclahr@users.noreply.github.com>
2019-05-22 10:14:27 +0200

    Edit code review comments

--- src/recipeList/RecipePreview.js
+++ src/recipeList/RecipePreview.js
@@ -1,41 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
-const Flexbox = styled.div`
+const StyledFlexbox = styled.div`
   display: flex;
   justify-content: center;
-  margin-bottom: 10px;
+  align-content: center;
+  margin: 5px;
 `;
 
 const StyledRecipePreview = styled.div`
   background-image: url("https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80");
-  background-size: 150px;
-  width: 150px;
-  height: 150px;
+  background-size: 175px;
+  width: 175px;
+  height: 175px;
   background-position: center center;
   background-repeat: no-repeat;
 `;
 
-const Title = styled.span`
-  color: white;
-  background: #3d9970;
-`;
+const StyledTitle = styled.h4`
+  margin: 0px;
 
-const StyledRecipePreviewCaption = styled.figcaption`
-  position: relative;
-  right: 110px;
-  top: 5px;
-  height: 20px;
-  color: white;
-  background: #3d9970;
+  span {
+    background-color: #3d9970;
+    color: white;
+  }
 `;
 
 export default function RecipePreview() {
   return (
-    <Flexbox>
+    <StyledFlexbox>
       <StyledRecipePreview>
-        <Title>Rezepttitel</Title>
+        <StyledTitle>
+          <span>Rezepttitel</span>
+        </StyledTitle>
       </StyledRecipePreview>
-    </Flexbox>
+    </StyledFlexbox>
   );
 }
