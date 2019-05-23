b21de43f42fa9c8a3448eb9beec6a6667896e60f
psclahr <48948915+psclahr@users.noreply.github.com>
2019-05-23 21:34:41 +0200

    Fetch from Frontend

--- src/createRecipe/InputIngredient.js
+++ src/createRecipe/InputIngredient.js
@@ -83,10 +83,19 @@ export default function InputIngredient() {
   function handleInputChange(newValue) {
     const newInputValue = newValue;
     setInputValue(newInputValue);
-    getIngredients(inputValue);
-    getIngredientSuggestion()
-      .then(res => res.json())
-      .then(data => console.log(data));
+    getIngredient();
+    //getIngredients(inputValue);
+    //getIngredientSuggestion()
+    //.then(res => res.json())
+    //.then(data => console.log(data));
+
+    function getIngredient() {
+      fetch(
+        `http://api.edamam.com/auto-complete?q=${inputValue}&limit=10&app_id=$702bbe7d&app_key=7470d6e6a4439eb58cae84ec6ebc10a7`
+      )
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
   }
 
   return (
