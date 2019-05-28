73bda44d90b041ab282855349c944e08237df8d8
psclahr <48948915+psclahr@users.noreply.github.com>
2019-05-28 16:56:47 +0200

    Add CreatePage Grid

--- src/createRecipe/CreatePage.js
+++ src/createRecipe/CreatePage.js
@@ -2,13 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import InputIngredient from "./InputIngredient";
 
-const CreatePageGrid = styled.div``;
+const CreatePageGrid = styled.div`
+  display: grid;
+  grid-template-rows: 100px auto auto;
+`;
 
 export default function CreatePage() {
   return (
     <CreatePageGrid>
-      <p>lorem Ipsum</p>
-      <InputIngredient />
+      <Title />
+      <Ingredient>
+        <InputIngredient />
+      </Ingredient>
+      <Description />
     </CreatePageGrid>
   );
 }
