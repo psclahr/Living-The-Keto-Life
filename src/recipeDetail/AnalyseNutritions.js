6439d2453c5833dfb17ca7bd973efd9f1571e085
psclahr <48948915+psclahr@users.noreply.github.com>
2019-06-07 12:24:18 +0200

    Pie Chart

--- src/recipeDetail/AnalyseNutritions.js
+++ src/recipeDetail/AnalyseNutritions.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { Pie } from "react-chartjs-2";
 
-export default function AnalyseNutritions({ recipe }) {}
+export default function AnalyseNutritions({ recipe }) {
+  const totalCalories = recipe.totalCalories;
+  const totalFats = recipe.totalFats;
+  console.log(totalFats);
+  const totalCarbs = recipe.totalCarbs;
+  const totalProteins = recipe.totalProteins;
+
+  const data = {
+    labels: ["Fats", "Carbs", "Proteins"],
+    datasets: [
+      {
+        label: "Nutrition Analyse",
+        backgroundColor: ["yellow", "red", "blue"],
+        borderColor: "black",
+        data: [totalFats, totalCarbs, totalProteins]
+      }
+    ]
+  };
+
+  return <Pie data={data} />;
+}
